7b54133616455448488f3fe74de4fa88cbae6373
KyHQ <hoquocky2000@gmail.com>
2023-06-10 16:30:09 +0700

    aaaa

--- QuocKy.cpp
+++ QuocKy.cpp
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-jkadjkdasfkd
-asdjkjkd
-asddfsd
-sddssd
-sdsdfdsff
 
-sdfsdfsd
-sdfsdfsd
-hehelwl
-wfewjd
-sdfsdf
-wmenenkjknwe
-sdfsdsdf
-asdaas
-asdsa
-asds
-=======
->>>>>>> parent of 75ade5e (add file QuocKy.cpp)
+
+
